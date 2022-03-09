turris-cz/os-updater-lists

commit 01444c9be017b5c7e304a139d7ed97446c499168

pci.lua: add support for Turris RTROM01-WS3294
This new miniPCIe pack [1] is using chipset: Mediatek MT7615

[1] https://www.discomp.cz/turris-rtrom01-ws3294-minipci-e-wifi-5ghz-card-for-mox-and-omnia_d113353.html

drivers/pci.lua
@@ -36,6 +36,11 @@ local db = {
 		device = 0x7612,
 		packages = {"kmod-mt76"}
 	},
+	{ -- MEDIATEK Corp. Device 7615
+                vendor = 0x7615,
+                device = 0x14c3,
+                packages = {"kmod-mt7615e"}
+	},
 }
 
 ----------------------------------------------------------------------------------

commit b32524e1236a1f76c185ee4ead3d05afd828b7da

drivers/pci.lua: fix for Turris RTROM01-WS3294
Unfortunately, this fault is done by the HW report, which I got from our
hardware department. They swapped vendor and device ID in the report and
I could not verify it. Since I did not have the mini PCIe card, I
could check it with the Mediatek MT7612, which we already have here
listed. :-(

Fixes: 01444c9be017b5c7e304a139d7ed97446c499168 ("pci.lua: add support for Turris RTROM01-WS3294")

drivers/pci.lua
@@ -37,8 +37,8 @@ local db = {
 		packages = {"kmod-mt76"}
 	},
 	{ -- MEDIATEK Corp. Device 7615
-                vendor = 0x7615,
-                device = 0x14c3,
+                vendor = 0x14c3,
+                device = 0x7615,
                 packages = {"kmod-mt7615e"}
 	},
 }
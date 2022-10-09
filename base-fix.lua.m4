turris-cz/os-updater-lists

commit 579aa856d96d03700637b149047d751196697d8c

base-fix: add fix-remote-access-ca-permissions
This commit ensures that package [1] is installed during the update.

[1] https://gitlab.nic.cz/turris/os/packages/-/commit/2f26d1be4789607917a3a3f47764e028bbb13731

base-fix.lua.m4
@@ -227,3 +227,10 @@ if os_release.VERSION and version_match(os_release.VERSION, "<=6.0.0") then
 	Install("fix-network-devices")
 	Package("fix-network-devices", { replan = "finished" })
 end
+
+-- Since OpenWrt 21.02 mosquitto runs under own user
+-- We need to change ownership of existing files to have it working for reForis Access plugin
+if version_match(os_release.VERSION, "<=6.0.0") then
+        Install("fix-remote-access-ca-permissions")
+        Package("fix-remote-access-ca-permissions", { replan = "finished" })
+end

commit 9646290552f4c2be6c4159dcd8a5ca00afd24edd

base-fix: add additional condition for fix-remote-access-ca-permissions
Related to: c5593c51a1025778d2afb4d5e238b74ce2290fc1
("base-fix: add additional condition for os_release.VERSION")

Fixes: 579aa856d96d03700637b149047d751196697d8c ("base-fix: add
fix-remote-access-ca-permissions")

base-fix.lua.m4
@@ -230,7 +230,7 @@ end
 
 -- Since OpenWrt 21.02 mosquitto runs under own user
 -- We need to change ownership of existing files to have it working for reForis Access plugin
-if version_match(os_release.VERSION, "<=6.0.0") then
+if os_release.VERSION and version_match(os_release.VERSION, "<=6.0.0") then
         Install("fix-remote-access-ca-permissions")
         Package("fix-remote-access-ca-permissions", { replan = "finished" })
 end
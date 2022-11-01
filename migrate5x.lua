e5e00072962c70d26f52ad6b0a3fad2540afb411
Josef Schlehofer <pepe.schlehofer@gmail.com>
2022-11-01 14:28:11 +0100

    migrate5x: add ath10k-firmware-qca9887-ct-htt
    This package was renamed in upstream in this commit [1]
    
    [1] https://github.com/openwrt/openwrt/commit/2e5e9b459ed53b42f8938294a98e2546be976c85#diff-2ab20efb63a815ff0ce453bc845bdb00c484ea2d1bfa229ba570c090183f8e41L103

--- migrate5x.lua
+++ migrate5x.lua
@@ -8,5 +8,6 @@ Package("kcptun-s", { virtual=true, deps = "kcptun-server" })
 Package("usbreset", { virtual=true, deps = "usbutils" })
 
 Package("wireguard", { virtual=true, deps = { "kmod-wireguard", "wireguard-tools" }})
+Package("ath10k-firmware-qca9887-ct-htt", { virtual=true, deps = { "ath10k-firmware-qca9887-ct-full-htt" }})
 Package("ath10k-firmware-qca9888-ct-htt", { virtual=true, deps = { "ath10k-firmware-qca9888-ct-full-htt" }})
 Package("foris", { virtual = true })
